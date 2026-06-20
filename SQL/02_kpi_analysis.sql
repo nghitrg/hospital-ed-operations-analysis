b8031e19dc37d1daf96ec112356528d68445709d
Nghi Truong <nghitrg@gmail.com>
2026-06-20 12:38:48 +0700

    Update 02_kpi_analysis.sql

--- SQL/02_kpi_analysis.sql
+++ SQL/02_kpi_analysis.sql
@@ -1,25 +1,27 @@
----1. Tổng số bệnh nhân 
-SELECT COUNT(patient_id) AS total_patients 
-FROM tdvnghi-00.dataset.hospital_operation_clean ;
+-- Key Performance Indicators (KPIs)
 
---- 2. Trung bình thời gian chờ cấp cứu
-SELECT AVG(patient_waittime) AS avg_waitime
-FROM tdvnghi-00.dataset.hospital_operation_clean 
+-- 1. Total number of patients
+SELECT COUNT(patient_id) AS total_patients
+FROM `tdvnghi-00.dataset.hospital_operation_clean`;
+
+-- 2. Average waiting time
+SELECT AVG(patient_waittime) AS avg_wait_time
+FROM `tdvnghi-00.dataset.hospital_operation_clean`
 WHERE patient_waittime IS NOT NULL;
 
---- 3. Trung bình độ hài lòng của bệnh nhân
-SELECT AVG(patient_satisfaction_score) avg_satisfaction_score
-FROM tdvnghi-00.dataset.hospital_operation_clean 
+-- 3. Average patient satisfaction score
+SELECT AVG(patient_satisfaction_score) AS avg_satisfaction_score
+FROM `tdvnghi-00.dataset.hospital_operation_clean`
 WHERE patient_satisfaction_score IS NOT NULL;
 
---- 4. Tỷ lệ nhập viện
-WITH admission_table
-AS
-(
-SELECT 
-  patient_admission_flag,
-  IF(patient_admission_flag = 'Admission',1,0) AS admission
-FROM tdvnghi-00.dataset.hospital_operation_clean 
+-- 4. Admission rate
+WITH admission_table AS (
+    SELECT
+        patient_admission_flag,
+        IF(patient_admission_flag = 'Admission', 1, 0) AS admission
+    FROM `tdvnghi-00.dataset.hospital_operation_clean`
 )
-SELECT ROUND(SUM(admission)*100.0/COUNT(*),2) AS admission_rate
+
+SELECT
+    ROUND(SUM(admission) * 100.0 / COUNT(*), 2) AS admission_rate
 FROM admission_table;
