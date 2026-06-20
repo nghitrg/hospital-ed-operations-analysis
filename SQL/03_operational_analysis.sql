0ad93d7feb78e8e50d7dec97d3aa787804166e03
Nghi Truong <nghitrg@gmail.com>
2026-06-20 12:40:08 +0700

    Update 03_operational_analysis.sql

--- SQL/03_operational_analysis.sql
+++ SQL/03_operational_analysis.sql
@@ -1,36 +1,38 @@
--- 5. Số lượng bệnh nhân theo giờ
+-- Operational Analysis
+
+-- 5. Patient volume by hour
 SELECT
-  EXTRACT(HOUR FROM patient_admission_time) AS admission_hour,
-  COUNT(*) AS total_patients
+    EXTRACT(HOUR FROM patient_admission_time) AS admission_hour,
+    COUNT(*) AS total_patients
 FROM `tdvnghi-00.dataset.hospital_operation_clean`
 GROUP BY admission_hour
 ORDER BY admission_hour;
 
--- 6. Số lượng bệnh nhân theo năm
+-- 6. Patient volume by year
 SELECT
-  EXTRACT(YEAR FROM patient_admission_date) AS admission_year,
-  COUNT(*) AS total_patients
+    EXTRACT(YEAR FROM patient_admission_date) AS admission_year,
+    COUNT(*) AS total_patients
 FROM `tdvnghi-00.dataset.hospital_operation_clean`
 GROUP BY admission_year
 ORDER BY admission_year;
 
--- 7. Số lượng bệnh nhân theo tháng
+-- 7. Patient volume by month
 SELECT
-  EXTRACT(MONTH FROM patient_admission_date) AS admission_month,
-  COUNT(*) AS total_patients
+    EXTRACT(MONTH FROM patient_admission_date) AS admission_month,
+    COUNT(*) AS total_patients
 FROM `tdvnghi-00.dataset.hospital_operation_clean`
 GROUP BY admission_month
 ORDER BY admission_month;
 
---- 8. Số lượng bệnh nhân theo Khoa
+-- 8. Patient volume by department
 SELECT 
-  department_referral, 
-  COUNT (*) AS total_patients
+    department_referral, 
+    COUNT(*) AS total_patients
 FROM `tdvnghi-00.dataset.hospital_operation_clean`
 WHERE department_referral IS NOT NULL
 GROUP BY department_referral
-ORDER BY total_patients DESC ;
+ORDER BY total_patients DESC;
 
---- Đa số bệnh nhân không cần chuyển đến khoa chuyên môn.
---- General Practice tiếp nhận số lượng bệnh nhân nhiều nhất.
---- Orthopedics có lượng bệnh nhân cao hơn đáng kể so với các khoa chuyên môn khác.
+-- Most patients did not require referral to a specialist department.
+-- General Practice handled the highest patient volume.
+-- Orthopedics had a significantly higher patient volume than other specialist departments.
